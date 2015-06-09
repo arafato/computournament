9bef38a7e9fe0753b13fffb00f376ccbf62ee1d4
Oliver Arafat <arafato@amazon.de>
2015-06-09 16:23:56 +0200

    added and integrated lambda function createExercise

--- client/app/js/vm.js
+++ client/app/js/vm.js
@@ -7,8 +7,13 @@ var vm = function(vm) {
     this.identityId = ko.observable();
     this.rank = ko.observable();
     this.level = ko.observable();
-    this.authError = ko.observable(false);
+    this.error = ko.observable(false);
+    this.errormsg = ko.observable("");
     this.pending = ko.observable(false);
+
+    this.challenge = ko.observable("");
+    
+    this.lambda = new AWS.Lambda();
     
     Config.getIdentityId(function(id) {
 	that.identityId(id);
@@ -18,13 +23,30 @@ var vm = function(vm) {
     });
 };
 
-vm.prototype.newChallenge = function() {
+vm.prototype.generateChallenge = function() {
+
+    var that = this;
+    
     this.pending(true);
     console.log("new challenge");
+
+    var params = {
+	FunctionName: "generateChallenge",
+	InvocationType: "RequestResponse"
+    };
+    this.lambda.invoke(params, function(err, data) {
+	if (err) {
+	    console.log(err, err.stack);
+	    that.error(true);
+	    that.errormsg(err);
+	} else {
+	    that.challenge(data.Payload);
+	}
+    });
 };
 
 vm.prototype.submitResult = function() {
 
     console.log("submitting challenge...");
     this.pending(false);
-}
+};
--- lambda/createExercise.js
+++ lambda/createExercise.js
@@ -1,9 +1,12 @@
-console.log('Loading function');
 
 exports.handler = function(event, context) {
-    console.log('event:', event);
-    var xnum = Math.round(Math.random()*41)- 20;
-    var ynum = Math.round(Math.random()*41)- 20;
-    var ex = xnum + ' + ' + ynum + ' = ?'
+
+    var xnum = Math.round(Math.random()*500)+1;
+    var ynum = Math.round(Math.random()*500)+1;
+    var op = (xnum % 2 === 0) ? " + " : " - "; 
+    var ex = xnum + op + ynum + ' = ?';
+
+    // TODO: Store challenge along with cognito id in DynamoDB
+    
     context.succeed(ex);
 };
