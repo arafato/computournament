b8c6df1f3841cfd439ffe98d0335c2d469df1a70
arafato <oliver.arafat@outlook.com>
2015-06-10 20:51:07 +0200

    integrated rank calculation

--- client/app/js/index.js
+++ client/app/js/index.js
@@ -30,28 +30,59 @@ $(function() {
 
 	(function poll(){
 	    setTimeout(function() {
-		
-		var params = {
-		    Key: {
-			CognitoId: {
-			    S: id
-			}
-		    },
-		    TableName: Config.LEADERSHIP_TABLE
-		};
-		
-		ddb.getItem(params, function(err, data) {
-		    if (err) {
-			model.error(true);
-			model.errormsg(err);
-			return;
-		    }
-
-		    model.score(data.Item.Score.N);
-		    poll();
-		});
+		analyseLeadershipBoard(id, poll);
 		
 	    }, Config.POLLING_INTERVAL);
 	})();
     });
+
+    function analyseLeadershipBoard(id, cb) {
+
+	var params = {
+	    TableName: Config.LEADERSHIP_TABLE,
+	    ReturnConsumedCapacity: "TOTAL"
+	};
+	
+	ddb.scan(params, function(err, data) {
+	    if (err) {
+		model.error(true);
+		model.errormsg(err);
+		return;
+	    }
+
+	    data.Items.sort(function(a, b){
+		return b.Score.N - a.Score.N;
+	    });
+
+	    var count = data.Count;
+	    var pos = data.Items.map(function(e) { return e.CognitoId.S; }).indexOf(id);
+	    var score = data.Items[pos].Score.N;
+	    	    
+	    model.score(score);
+	    model.rank(pos+1 + " / " + count);
+	    cb();
+	});
+    }
 });
+
+
+
+// var params = {
+// 	    Key: {
+// 		CognitoId: {
+// 		    S: id
+// 		}
+// 	    },
+// 	    TableName: Config.LEADERSHIP_TABLE
+// 	};
+	
+// 	ddb.getItem(params, function(err, data) {
+// 	    if (err) {
+// 		model.error(true);
+// 		model.errormsg(err);
+// 		return;
+// 	    }
+	    
+// 	    model.score(data.Item.Score.N);
+// 	    cb();
+// 	});
